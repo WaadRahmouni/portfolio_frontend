f931a2fd041e98e39d5a69fde74beb1ca81569b2
Waad <waadrahmouni05@gmail.com>
2025-12-26 20:52:35 +0100

    first commit11

--- src/services/educationService.js
+++ src/services/educationService.js
@@ -1,36 +1,29 @@
 import axios from "axios";
 
-const API_URL = "/api/education";
+const API_URL = `${process.env.REACT_APP_API_URL}/api/education`;
 
 export const getEducation = async () => {
   const res = await axios.get(API_URL);
   return res.data;
 };
 
-// (optionnel si tu veux un dashboard admin)
 export const createEducation = async (data) => {
   const res = await axios.post(API_URL, data, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
   });
   return res.data;
 };
 
 export const updateEducation = async (id, data) => {
   const res = await axios.put(`${API_URL}/${id}`, data, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
   });
   return res.data;
 };
 
 export const deleteEducation = async (id) => {
   const res = await axios.delete(`${API_URL}/${id}`, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
   });
   return res.data;
 };
